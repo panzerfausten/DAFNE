e293ff81076c94b303285fd846accf18ddc931d2
Darien Miranda <panzerfausten@gmail.com>
2019-10-12 09:15:12 -0700

    solved line issue

--- dafne/src/components/DafnePlot.js
+++ dafne/src/components/DafnePlot.js
@@ -23,6 +23,8 @@ class DafnePlot extends React.Component {
     this.height = 0;
     this.svg    = null;
     this.renderPlot = this.renderPlot.bind(this);
+    this.drawAxis = this.drawAxis.bind(this);
+
   }
   componentDidMount(){
     this.dafnePlot.addEventListener('resize', (event) => console.log(event.detail));
@@ -35,7 +37,7 @@ class DafnePlot extends React.Component {
     this.clear();
     //re-set the size
     this.width  = this.svgW - 90;
-    this.height = this.svgH;
+    this.height = this.svgH - this.margin.bottom;
     // append the svg object to the body of the page
     this.svg = d3.select("#dafne_plot")
     .append("svg")
@@ -47,50 +49,47 @@ class DafnePlot extends React.Component {
     //------------------------------------------------------------------------//
 
     // let _this = this;
-    var data = [10,3];
+    let domain = ["A","B","C","D","E","F"];
+    var data = [9,1,2,3,2];
+    var lineData = [
+      {"name":"A","value":9},
+      {"name":"B","value":1},
+      {"name":"C","value":2},
+      {"name":"D","value":3},
+      {"name":"E","value":2},
+    ]
     var x = d3.scalePoint()
-              .domain(["A","B","C"])
+              .domain(domain)
               .range([0,this.width]);
     var y = d3.scaleLinear()
               .domain([0,10])
               .range([0,this.height]);
-    this.svg.append("circle")
-        .attr("cx",x("B"))
-        .attr("cy",this.height - this.margin.bottom)
-        .attr("r",8)
-    this.svg.append("circle")
-        .attr("cx",x("A"))
-        .attr("cy",((this.height / 2) - this.margin.bottom))
-        .attr("r",8)
-    // var x = d3.scaleLinear()
-    //   .domain([ 0 , 10 ])
-    //   .range([ 0 , 10]);
-    // // Add Y axis
-    // var y = d3.scaleLinear()
-    //   .domain([0, 10])
-    //   .range([ this.height - this.margin.bottom, 0 ]);
-    //
-    // //draw axis
-    // this.svg.append("g")
-    //   .call(d3.axisLeft(y));
-    this.svg.append("g")
-      .attr("transform", `translate(${0}, 0)`)
-      .call(d3.axisLeft(y));
-    this.svg.append("g")
-      .attr("transform", `translate(${this.width / 2 }, 0)`)
-      .call(d3.axisLeft(y));
-    // // draw the line
-    // this.svg.append("path")
-    //  .datum(data)
-    //  .attr("fill", "none")
-    //  .attr("stroke", "steelblue")
-    //  .attr("stroke-width", 1.5)
-    //  .attr("d", d3.line()
-    //    .x(function(d) { return x(d) })
-    //    .y(function(d) { return y(d) })
-    //  )
+              var line = d3.line()
+              	.x(function(d){ return x(d.name)})
+              	.y(function(d){ return y(d.value)});
+
+                this.svg.append("path")
+                	.attr("d", line(lineData))
+                	.attr("stroke", "teal")
+                	.attr("stroke-width", "2")
+                	.attr("fill", "none");
+
+    for (var i = 0; i < data.length; i++) {
+      this.svg.append("circle")
+          .attr("cx",x(domain[i]))
+          .attr("cy",y(data[i]))
+          .attr("r",8)
+      this.svg.append("g")
+        .attr("transform", `translate(${(this.width / data.length) * i }, 0)`)
+        .call(d3.axisLeft(y));
+      this.drawAxis();
 
 
+    }
+
+  }
+  drawAxis(){
+    // this.svg.append('rect').attrs({ x: 0, y: 0, width: 20, height: 80, fill: 'red' })
   }
   onResize = (w,h) => {
     this.svgW = w;
