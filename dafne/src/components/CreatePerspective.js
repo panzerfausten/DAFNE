fee81392cb347b37812796a96ea68f9928cc30e0
Darien Miranda <panzerfausten@gmail.com>
2019-10-16 21:39:36 -0700

    pathway style

--- dafne/src/components/CreatePerspective.js
+++ dafne/src/components/CreatePerspective.js
@@ -2,6 +2,8 @@ import React from 'react';
 import DafnePlot from "../components/DafnePlot";
 import IndicatorTools from '../components/IndicatorTools';
 import PathwaysList from '../components/PathwaysList';
+import 'rc-checkbox/assets/index.css';
+import Checkbox  from 'rc-checkbox';
 
 class CreatePerspective extends React.Component {
   handleChange(event) {
@@ -17,9 +19,39 @@ class CreatePerspective extends React.Component {
                 <div className="widget_title">
                   Solution Pathways
                 </div>
-                <div className="widget_content">
-                  <div className="filters_area">
+                <div className="filter">
+                  <div className="filter_title">
+                    Pathway tools
                   </div>
+                  <div clasName="filter_content">
+                    <div className="filter_row">
+                      <label className="filter_label" >
+                        <Checkbox
+                          defaultChecked
+                          onChange={() => {}}
+                          disabled={false}
+                        />
+                        Show favourite pathways
+                      </label>
+                      <div>i</div>
+                      <div>i</div>
+                    </div>
+                    <div className="filter_row">
+                      <label className="filter_label" >
+                        <Checkbox
+                          defaultChecked
+                          onChange={() => {}}
+                          disabled={false}
+                        />
+                        Show average alternatives
+                      </label>
+                      <div>i</div>
+                      <div>i</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="widget_content">
+                  <div className="solution_list_title">Solution pathways and their impact on the selected indicators:</div>
                   <PathwaysList></PathwaysList>
                 </div>
               </div>
--- dafne/src/components/Pathway.js
+++ dafne/src/components/Pathway.js
@@ -11,6 +11,14 @@ class Pathway extends React.Component {
         <div style={{flex:1,backgroundColor:"#bcbec0",paddingLeft:15}}>
           {`Solution pathway ${this.props.item}`}
         </div>
+        <div className="arrow-filler" style={{
+                                              backgroundColor:"#bcbec0",
+                                              height:24,
+                                              display:"flex",
+                                              alignItems:"center"}}>
+          <div className="arrow-down">
+          </div>
+        </div>
         <div className="arrow-right">
         </div>
       </div>
