81ccf27265caa44f3e21320179c193b7dc3b4516
America Mendoza <america@Americas-Mac-mini.local>
2019-10-22 18:13:51 -0700

    modal indicators

---
+++ dafne/src/components/IndicatorFilter.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import "../style/style.scss";
+import { Modal, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+class IndicatorFilter extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+  }
+
+  handleOpenModal(value){
+    this.props.handleOpenModal(value);
+  }
+
+  render(){
+    return (
+      <Modal className='custom_modal' show={this.props.show} onHide={() => this.handleOpenModal(false)} centered>
+        <Modal.Header closeButton className="custom_modal_header">
+          <Modal.Title> Select your indicators </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='custom_modal_content'>
+            <div className='custom_top_content m-b-10 p-10'>
+              <span>Filters: </span>
+
+              <div>
+                <span>Region</span>
+                <select className='m-l-5'>
+                  <option>basin-wide</option>
+                </select>
+              </div>
+
+              <div>
+                <span>Sector</span>
+                <select className='m-l-5'>
+                  <option>All</option>
+                </select>
+              </div>
+            </div>
+
+            <div className='custom_bottom_content p-10'>
+              <select multiple>
+                <option>Energy Gibe 1</option>
+                <option>Energy Gibe 2</option>
+                <option>Energy Gibe 3</option>
+                <option>Energy K</option>
+              </select>
+            </div>
+          </div>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className='btn_modal' block onClick={() => this.handleOpenModal(false)}>
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+IndicatorFilter.propTypes = {
+  show            : PropTypes.bool,
+  handleOpenModal : PropTypes.func,
+  onClick         : PropTypes.func
+};
+
+
+IndicatorFilter.defaultProps = {
+  show            : false,
+  handleOpenModal : () => {},
+  onClick         : () => {}
+};
+
+export default IndicatorFilter;
