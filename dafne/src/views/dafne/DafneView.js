e01f24380301188fa54148499730474540e0959c
Darien Miranda <dmiranda@nodeport.co>
2020-02-10 11:11:47 -0800

    added authorization redirection

--- dafne/src/views/dafne/DafneView.js
+++ dafne/src/views/dafne/DafneView.js
@@ -14,14 +14,23 @@ class DafneView extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      selectedIndex:1
+      selectedIndex:1,
+      authorized:false
     }
     this.handleChange = this.handleChange.bind(this);
     this.changeIndex  = this.changeIndex.bind(this);
 
   }
   componentDidMount(){
-    console.log("props",this.props.match.params.id);
+    DafneApi.getMe().then( (res) => {
+      if(!res.valid_token){ //redirect to profile if user is logged in
+        this.redirectHome();
+      }else{
+        this.setState({
+          authorized:true
+        })
+      }
+    });
   }
   handleChange(event) {
     this.setState({[event.target.name]: event.target.value});
@@ -50,25 +59,30 @@ class DafneView extends React.Component {
     });
   }
   render(){
-    return (
-      <div className="container-fluid flex">
-        <Navbar  expand="lg">
-          <Navbar.Brand href="#home"><img src={DafneLogo} alt=""></img></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link onClick={() => this.changeIndex(0)} className={this.state.selectedIndex === 0 ? 'navbar-selected' : null}>Create your perspective</Nav.Link>
-              <Nav.Link onClick={() => this.changeIndex(1)} className={this.state.selectedIndex === 1 ? 'navbar-selected' : null}>Compare perspectives</Nav.Link>
-            </Nav>
-            <NavDropdown title="Account" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => this.logout()}>Log out</NavDropdown.Item>
-            </NavDropdown>
-          </Navbar.Collapse>
-        </Navbar>
-        {this.renderContent()}
+    if(this.state.authorized){
+      return (
+        <div className="container-fluid flex">
+          <Navbar  expand="lg">
+            <Navbar.Brand href="#home"><img src={DafneLogo} alt=""></img></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link onClick={() => this.changeIndex(0)} className={this.state.selectedIndex === 0 ? 'navbar-selected' : null}>Create your perspective</Nav.Link>
+                <Nav.Link onClick={() => this.changeIndex(1)} className={this.state.selectedIndex === 1 ? 'navbar-selected' : null}>Compare perspectives</Nav.Link>
+              </Nav>
+              <NavDropdown title="Account" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={() => this.logout()}>Log out</NavDropdown.Item>
+              </NavDropdown>
+            </Navbar.Collapse>
+          </Navbar>
+          {this.renderContent()}
 
-      </div>
-    )
+        </div>
+      )
+    }else{
+      return null;
+    }
+
   }
 
 }
