7618ffdfeca4aa9b907d6781859b155c28628085
Darien Miranda <panzerfausten@gmail.com>
2020-07-07 20:54:16 -0700

    change password api

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,5 @@ router.post('/login' ,userController.login);
 router.post('/create',userController.create);
 router.get('/logout',userController.logout);
 router.get('/me',userController.me);
-
+router.post('/change_password',userController.change_password)
 module.exports = router;
